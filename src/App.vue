5ab45bae17c2bb5afbfab13a3ea9a69f3d9c52cc
Beckie Liwacz <beckieliwacz@gmail.com>
2021-08-01 17:48:04 -0400

    completed Vue todo list app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <todo-list />
+    <new-todo />
+    <todo-summary />
+  </div>
+</template>
+
+<script>
+import TodoSummary from './components/TodoSummary.vue'
+import TodoList from './components/TodoList.vue'
+import NewTodo from './components/NewTodo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+    NewTodo,
+    TodoSummary
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
